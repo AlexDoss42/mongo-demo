bb3d5edaeda0eba501495293d30bb56b5e95aebd
Alex Doss <alex.doss42@gmail.com>
2020-06-26 19:24:53 -0600

    adds course schema

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('could not connect to MongoDB', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
