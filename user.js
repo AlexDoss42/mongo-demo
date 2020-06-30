ea0e5dc4d2d3ad2516f8f9426251e841ddb64946
Alex Doss <alex.doss42@gmail.com>
2020-06-29 21:36:08 -0600

    adds validateuser function and exports

--- user.js
+++ user.js
@@ -22,3 +22,16 @@ const User = mongoose.model('User', new mongoose.Schema({
     maxlength: 1024
   }
 }));
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+};
+
+exports.User = User;
+exports.validate = validateUser;
