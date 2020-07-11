be26cc7dc4fbfeeb019fe8064d6899900a804f07
Alex Doss <alex.doss42@gmail.com>
2020-07-11 16:44:29 -0600

    adds jasonwebtoken

---
+++ routes/auth.js
@@ -0,0 +1,23 @@
+const _ = require('lodash');
+const jwt = require('jasonwebtoken');
+const bcrypt = require('bcrypt');
+const {User, validate} = require('../user');
+const mongoose = require('mongoose');
+const express = require('express');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const router = express.Router();
+
+router.post('/', async(req, res) =>{
+ const { error } = validate(req.body);
+if (error) return res.status(400).send(error.detail[0].message);
+
+let user = await User.findOne({ email: req.body.email });
+if (user) return res.status(400).send("User already registered.");
+
+const validPassword = await bcrypt.compare(req.body.password, user.password);
+if (!validPassword) return res.status(400).send("Invalid email or password");
+
+jwt.sign()
+
+res.send(true);
+});
